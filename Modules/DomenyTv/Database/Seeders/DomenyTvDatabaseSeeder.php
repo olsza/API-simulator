913c1d8ec11cc016331caeb6dab5341c0b747736
Olsza <olsza@users.noreply.github.com>
2024-03-05 20:15:10 +0100

    add clear module "DomenyTv"

---
+++ Modules/DomenyTv/Database/Seeders/DomenyTvDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\DomenyTv\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DomenyTvDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
---
+++ Modules/DomenyTv/routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\DomenyTv\App\Http\Controllers\DomenyTvController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group([], function () {
+    Route::resource('domenytv', DomenyTvController::class)->names('domenytv');
+});
