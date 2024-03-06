0efb949d93f9ec40f27d26ec3efb23cf0e7bd2b0
Olsza <olsza@users.noreply.github.com>
2024-03-06 17:17:00 +0100

    create AccountBalanceTest

---
+++ Modules/DomenyTv/tests/Feature/AccountBalanceTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Modules\DomenyTv\tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AccountBalanceTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function testExample(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
