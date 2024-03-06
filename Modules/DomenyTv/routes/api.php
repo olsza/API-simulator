5d79c3a447f585550feb581d854ff65cc34e1ef4
Olsza <olsza@users.noreply.github.com>
2024-03-06 17:16:23 +0100

    disable the "auth:sanctum" routes for the DomainTv module

--- Modules/DomenyTv/routes/api.php
+++ Modules/DomenyTv/routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
     |
 */
 
-Route::middleware(['auth:sanctum'])->name('api.')->group(function () {
+Route::middleware([])->name('api.')->group(function () {
     Route::get('domenytv', fn (Request $request) => $request->user())->name('domenytv');
 });
