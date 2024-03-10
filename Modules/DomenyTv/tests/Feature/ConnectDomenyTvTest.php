afbb5664c55e67931b93f42c577bb8963715527e
Olsza <olsza@users.noreply.github.com>
2024-03-10 12:34:38 +0100

    create ConnectDomenyTv feature test for DomenyTv module

---
+++ Modules/DomenyTv/tests/Feature/ConnectDomenyTvTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Modules\DomenyTv\tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ConnectDomenyTvTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function testExampleDomenyTvModule(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
