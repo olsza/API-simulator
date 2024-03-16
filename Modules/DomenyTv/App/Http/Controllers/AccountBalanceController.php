ba4110471ea74a5d6ece02d17d68cf2849286e38
Olsza <olsza@users.noreply.github.com>
2024-03-16 13:45:15 +0100

    refactoring after pint for AccountBalanceController.php in DomenyTv module

--- Modules/DomenyTv/App/Http/Controllers/AccountBalanceController.php
+++ Modules/DomenyTv/App/Http/Controllers/AccountBalanceController.php
@@ -5,7 +5,6 @@ namespace Modules\DomenyTv\App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
 class AccountBalanceController extends Controller
 {
