d9f23c6e04ab0b7168496f44423a9cf5d5e1160d
Olsza <olsza@users.noreply.github.com>
2024-03-18 00:13:24 +0100

    create AccountBalanceRequest in DomenyTv module

---
+++ Modules/DomenyTv/App/Http/Requests/AccountBalanceRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Modules\DomenyTv\App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AccountBalanceRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+}
