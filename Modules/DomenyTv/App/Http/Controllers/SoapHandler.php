4f11d47813edd203819ef7a75cfcc6532c972588
Olsza <olsza@users.noreply.github.com>
2024-03-20 18:56:01 +0100

    create SoapHandler with 2 commends - draft

---
+++ Modules/DomenyTv/App/Http/Controllers/SoapHandler.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Modules\DomenyTv\App\Http\Controllers;
+
+class SoapHandler
+{
+    public function accountBalance($login, $password)
+    {
+        return ['result' => 1000, 'balance' => (string) 3210.66]; // Let us assume that the balance is 3210.66
+    }
+
+    public function checkDomainDns($login, $password, $domain = null)
+    {
+        return ['result' => 1000, [
+            'dns1' => 'dns.' . ($domain ?: 'olsza.info'),
+            'dns2' => 'dns2.' . ($domain ?: 'czlowiek.it'),
+            'dns3' => 'dns3.' . ($domain ?: 'domeny.tv'),
+        ]];
+    }
+}
