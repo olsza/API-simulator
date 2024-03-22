28878a95119d8f103c8ae54ee81911a502ca72e9
Olsza <olsza@users.noreply.github.com>
2024-03-22 18:33:06 +0100

    change test in AccountBalanceTest - add "test_good_value_balance"

--- Modules/DomenyTv/tests/Feature/AccountBalanceTest.php
+++ Modules/DomenyTv/tests/Feature/AccountBalanceTest.php
@@ -2,72 +2,67 @@
 
 namespace Modules\DomenyTv\tests\Feature;
 
-use SoapClient;
-use Tests\TestCase;
+use GuzzleHttp\Client;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
 
 class AccountBalanceTest extends TestCase
 {
-    const FILE_XML = 'soap.wdl.xml';
+    private string $url = 'http://localhost/api/soap';
+
+    public function test_good_value_balance()
+    {
+        $body = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://localhost">
+                    <soapenv:Header/>
+                    <soapenv:Body>
+                        <ser:accountBalance>
+                            <input>
+                                <login>good_login</login>
+                                <password>good_pass</password>
+                            </input>
+                        </ser:accountBalance>
+                    </soapenv:Body>
+                </soapenv:Envelope>';
+
+        $response = $this->sendUrlPost($body);
+
+        $responseBody = $response->getBody()->getContents();
+
+        $expectedResponse = '<?xml version="1.0" encoding="UTF-8"?>
+                            <SOAP-ENV:Envelope
+                                xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
+                                xmlns:ns1="urn:xmethods-delayed-quotes"
+                                xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                                xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+                                xmlns:ns2="http://xml.apache.org/xml-soap"
+                                xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+                                <SOAP-ENV:Body>
+                                    <ns1:accountBalanceResponse>
+                                        <return xsi:type="ns2:Map">
+                                            <item>
+                                                <key xsi:type="xsd:string">result</key>
+                                                <value xsi:type="xsd:int">1000</value>
+                                            </item>
+                                            <item>
+                                                <key xsi:type="xsd:string">balance</key>
+                                                <value xsi:type="xsd:string">3210.66</value>
+                                            </item>
+                                        </return>
+                                    </ns1:accountBalanceResponse>
+                                </SOAP-ENV:Body>
+                            </SOAP-ENV:Envelope>';
+
+        $this->assertXmlStringEqualsXmlString($expectedResponse, $responseBody);
+    }
 
     /**
-     * A basic feature test example.
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function testExample(): void
+    public function sendUrlPost(string $body): ResponseInterface
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
-
-    public function testCheckDomenyTv(): void
-    {
-        $response = $this->get(route('api.domenytv'));
-
-        $response->assertStatus(200);
-    }
-
-    public function testReadSoapWdlXmlFile()
-    {
-        $client = $this->connectWithServerWdl();
-
-        $this->assertCount(52, $client->__getFunctions());
-    }
-
-    public function testCheckIsAccountBalanceFunctionIsInSoapWdlXmlFile()
-    {
-        $client = $this->connectWithServerWdl();
-
-        $commandExists = false;
-        foreach ($client->__getFunctions() as $function) {
-            if (str_contains($function, 'accountBalance')) {
-                $commandExists = true;
-
-                break;
-            }
-        }
-
-        $this->assertTrue($commandExists);
-    }
-
-    /**
-     * @throws \SoapFault
-     */
-    private function connectWithServerWdl(): SoapClient
-    {
-        return new SoapClient(route('api.domenytv') . '/' . self::FILE_XML, $this->optionToConnectWdl());
-    }
-
-    private function optionToConnectWdl()
-    {
-        return [
-            'stream_context' => stream_context_create([
-                'ssl' => [
-                    'verify_peer' => false,
-                    'verify_peer_name' => false,
-                    'allow_self_signed' => true,
-                ],
-            ]),
-            'cache_wsdl' => WSDL_CACHE_NONE,
-        ];
+        return (new Client())->post($this->url, [
+            'headers' => ['Content-Type' => 'text/xml'],
+            'body' => $body,
+        ]);
     }
 }
