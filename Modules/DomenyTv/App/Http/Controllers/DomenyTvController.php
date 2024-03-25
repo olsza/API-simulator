1c93e0d39e969d326427663aa313f5da5bcfd6c0
Olsza <olsza@users.noreply.github.com>
2024-03-25 20:50:54 +0100

    add check IP

--- Modules/DomenyTv/App/Http/Controllers/DomenyTvController.php
+++ Modules/DomenyTv/App/Http/Controllers/DomenyTvController.php
@@ -5,22 +5,29 @@ namespace Modules\DomenyTv\App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use SoapServer;
+use Symfony\Component\HttpFoundation\IpUtils;
 
 class DomenyTvController extends Controller
 {
     public function handle(Request $request)
     {
-        $server = new SoapServer(null, ['uri' => 'urn:xmethods-delayed-quotes']);
-        //        $server = new SoapServer(public_path('/modules/domenytv/soap.wsdl.xml'));
+        $checkIp = $this->checkIp();
 
-        // Define a class that supports SOAP operations
-        $server->setClass(SoapHandler::class);
+        if (! $checkIp) {
+            $server = new SoapServer(null, ['uri' => 'urn:xmethods-delayed-quotes']);
+            //        $server = new SoapServer(public_path('/modules/domenytv/soap.wsdl.xml'));
 
-        // SOAP request
-        ob_start();
-        $server->handle($this->parseRequest($request->getContent()));
-        //        $server->handle($request->getContent());
-        $response = ob_get_clean();
+            // Define a class that supports SOAP operations
+            $server->setClass(SoapHandler::class);
+
+            // SOAP request
+            ob_start();
+            $server->handle($this->parseRequest($request->getContent()));
+            //        $server->handle($request->getContent());
+            $response = ob_get_clean();
+        } else {
+            $response = $checkIp;
+        }
 
         return response($response, 200)->header('Content-Type', 'text/xml');
     }
@@ -34,5 +41,27 @@ class DomenyTvController extends Controller
         }
 
         return $xmlRequest;
+    }
+
+    private function checkIp(): false | string
+    {
+
+        $ip = request()->ip();
+        if (! IpUtils::checkIp($ip, ['172.0.0.0/8', '127.0.0.1'])) {
+            //            $server = new SoapServer(null, ['uri' => 'urn:xmethods-delayed-quotes']);
+            $outError = '<SOAP-ENV:Envelope
+	xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
+	<SOAP-ENV:Body>
+		<SOAP-ENV:Fault>
+			<faultcode>69</faultcode>
+			<faultstring>Unauthorized IP - ' . $ip . '</faultstring>
+		</SOAP-ENV:Fault>
+	</SOAP-ENV:Body>
+</SOAP-ENV:Envelope>';
+
+            return $outError;
+        }
+
+        return false;
     }
 }
