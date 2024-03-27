d3ae3b3a5679b80cc7bffe4eb14229fcd13fc197
Olsza <olsza@users.noreply.github.com>
2024-03-27 17:25:48 +0100

    change read parameters after changing dynamic parameters

--- Modules/DomenyTv/App/Http/Controllers/SoapHandler.php
+++ Modules/DomenyTv/App/Http/Controllers/SoapHandler.php
@@ -2,40 +2,49 @@
 
 namespace Modules\DomenyTv\App\Http\Controllers;
 
+use SoapFault;
+
 class SoapHandler
 {
-    public function accountBalance($login, $password)
+    public function handleRequest($methodName, $parameters) {
+        if (method_exists($this, $methodName)) {
+            return $this->$methodName($parameters);
+        } else {
+            throw new SoapFault("Server", "Method not found: $methodName");
+        }
+    }
+    public function accountBalance($parameters)
     {
-        if ($login !== 'good_login' && $password !== 'good_password') {
+        if ($parameters->login !== 'good_login' && $parameters->password !== 'good_password') {
             return ['result' => 27];
         }
 
         return ['result' => 1000, 'balance' => (string) 3210.66]; // Let us assume that the balance is 3210.66
     }
 
-    public function checkDomainExtended($login, $password, ...$part)
+    public function checkDomainExtended($parameters)
     {
-        if ($login !== 'good_login' && $password !== 'good_password') {
+        if ($parameters->login !== 'good_login' && $parameters->password !== 'good_password') {
             return ['result' => 27];
         }
 
-        if (! isset($part['domain'])) {
+        if (! isset($parameters->domain)) {
             return ['result' => 16];
         }
 
-        if (empty($part['domain'])) {
+        if (empty($parameters->domain)) {
             return ['result' => 1];
         }
 
         return ['result' => 1000, 'balance' => (string) 3210.66]; // Let us assume that the balance is 3210.66
     }
 
-    public function checkDomainDns($login, $password, $domain = null)
+    public function checkDomainDns($parameters)
     {
         return ['result' => 1000, [
-            'dns1' => 'dns.' . ($domain ?: 'olsza.info'),
-            'dns2' => 'dns2.' . ($domain ?: 'czlowiek.it'),
-            'dns3' => 'dns3.' . ($domain ?: 'domeny.tv'),
+            'dns1' => 'dns.' . ($parameters->domain ?: 'olsza.info'),
+            'dns2' => 'dns2.' . ($parameters->domain ?: 'czlowiek.it'),
+            'dns3' => 'dns3.' . ($parameters->domain ?: 'domeny.tv'),
         ]];
     }
 }
